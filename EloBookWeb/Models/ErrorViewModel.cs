bff28d96f0f751e503f00a6fc43bec645f9f1298
Elochukwu Aroh <elochukwuaroh@gmail.com>
2023-03-22 11:52:11 -0500

    Add project files.

---
+++ EloBookWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EloBookWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
